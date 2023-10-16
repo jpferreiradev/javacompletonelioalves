f142849facad451ba759e58a9a17a61618911f44
JP Ferreira <jotepauder01@gmail.com>
2023-10-16 19:26:56 -0300

    Update atividades

--- javacompletonelioalves/src/EstruturaCondicional/Atividades/Question_4.java
+++ javacompletonelioalves/src/EstruturaCondicional/Atividades/Question_4.java
@@ -3,16 +3,30 @@ package EstruturaCondicional.Atividades;
 import java.util.Scanner;
 
 public class Question_4 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
-        int valorHora;
-        int valorMinuto;
+        int valorPrimeiraHora;
+        int valorSegundaHora;
+        int calculoHoraMinuto;
+        int valorDia = 24;
+
 
         System.out.println("Digite o valor da hora e do minuto: ");
-        valorHora = scan.nextInt();
-        valorMinuto = scan.nextInt();
+        valorPrimeiraHora = scan.nextInt();
+        valorSegundaHora = scan.nextInt();
+
+
+        if(valorPrimeiraHora > valorSegundaHora){
+            calculoHoraMinuto = valorDia - (valorPrimeiraHora - valorSegundaHora);
+            System.out.println("O jogo durou " + calculoHoraMinuto + "horas");
+        } else if (valorSegundaHora > valorPrimeiraHora){
+            calculoHoraMinuto = valorPrimeiraHora - valorSegundaHora;
+            System.out.println("O jogo durou " + calculoHoraMinuto + "horas");
+        } else if (valorPrimeiraHora == 0 && valorSegundaHora == 0) {
+            System.out.println("O jogo durou 24hs");
+        }
 
 
     }
--- javacompletonelioalves/src/EstruturaCondicional/Atividades/Question_6.java
+++ javacompletonelioalves/src/EstruturaCondicional/Atividades/Question_6.java
@@ -3,6 +3,7 @@ package EstruturaCondicional.Atividades;
 public class Question_6 {
     public static void main(String[] args) {
 
+        
 
     }
 }
