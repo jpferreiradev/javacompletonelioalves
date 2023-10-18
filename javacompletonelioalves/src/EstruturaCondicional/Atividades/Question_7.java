db28a045a7f3a227a1451ebd0b9719030349ddf4
JP Ferreira <jotepauder01@gmail.com>
2023-10-18 14:04:51 -0300

    Update atividades

--- javacompletonelioalves/src/EstruturaCondicional/Atividades/Question_7.java
+++ javacompletonelioalves/src/EstruturaCondicional/Atividades/Question_7.java
@@ -15,7 +15,21 @@ public class Question_7 {
         x = scan.nextDouble();
         y = scan.nextDouble();
 
-
+        if(x == 0.0 && y == 0.0){
+            System.out.println("Origem");
+        } else if (x == 0.0) {
+            System.out.println("Eixo Y");
+        } else if (y == 0.0) {
+            System.out.println("Eixo x");
+        } else if (x > 0.0 && y > 0.0) {
+            System.out.println("Q1");
+        } else if (x < 0.0 && y > 0.0) {
+            System.out.println("Q2");
+        } else if (x < 0.0 && y < 0.0) {
+            System.out.println("Q3");
+        } else {
+            System.out.println("Q4");
+        }
 
 
     }
--- javacompletonelioalves/src/EstruturaCondicional/Atividades/Question_8.java
+++ javacompletonelioalves/src/EstruturaCondicional/Atividades/Question_8.java
@@ -14,7 +14,20 @@ public class Question_8 {
         System.out.println("Informe o seu salário");
         salario = scan.nextDouble();
 
-
-
+        if(salario <= 2000.0){
+            calculoImposto = 0.0;
+        } else if (salario <= 3000.0) {
+            calculoImposto = (salario - 2000.0) * 0.08;
+        } else if (salario <= 4500.0) {
+            calculoImposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
+        } else {
+            calculoImposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+        }
+        if(calculoImposto == 0.0){
+            System.out.println("Isento");
+        }
+        else{
+            System.out.printf("R$ %.2f%n", calculoImposto);
+        }
     }
 }
