ec33bd761d50f16d45458fc4771c9a583b11bfbe
JP Ferreira <jotepauder01@gmail.com>
2023-12-13 17:00:25 -0300

    Update curso Java

---
+++ javacompletonelioalves/src/Vetores/Atividades/terceiroExercicio.java
@@ -0,0 +1,8 @@
+package Vetores.Atividades;
+
+public class terceiroExercicio {
+    public static void main(String[] args) {
+
+
+    }
+}
--- javacompletonelioalves/src/Vetores/ExempleOne.java
+++ javacompletonelioalves/src/Vetores/ExempleOne.java
@@ -3,23 +3,23 @@ package Vetores;
 import java.util.Scanner;
 
 public class ExempleOne {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
         int n = scan.nextInt();
-        double [] vect = new double[n];
+        double[] vect = new double[n];
 
-        for(int i = 0; i < n;i++){ // vetor para pegar as notas
+        for (int i = 0; i < vect.length; i++) { // vetor para pegar as notas
             vect[i] = scan.nextDouble();
 
         }
 
         double sum = 0.0;
-        for(int i =0; i < n;i++){ // vetor para somar as notas
+        for (int i = 0; i < vect.length; i++) { // vetor para somar as notas
             sum += vect[i];
         }
-        double avg = sum / n; //
+        double avg = sum / vect.length; // divisao dos numeros
 
         System.out.printf("AVERAGE HEIGHT: %.2f%n", avg);
 
