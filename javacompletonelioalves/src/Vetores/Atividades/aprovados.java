db3d034de2c8eabbfb37f721f6af283c562dfe47
JP Ferreira <jotepauder01@gmail.com>
2024-01-03 16:26:20 -0300

    Update curso Java

--- javacompletonelioalves/src/Vetores/Atividades/aprovados.java
+++ javacompletonelioalves/src/Vetores/Atividades/aprovados.java
@@ -1,4 +1,33 @@
 package Vetores.Atividades;
 
+import java.util.Scanner;
+
 public class aprovados {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Quantos alunos serão digitados? ");
+        int n = scan.nextInt();
+
+        String nomes[] = new String[n];
+        double primeiraNota[] = new double[n];
+        double segundaNota[] = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Digite nome, primeira e segunda nota do" + (i + 1) + "aluno");
+            scan.nextLine();
+            nomes[i] = scan.nextLine();
+            primeiraNota[i] = scan.nextDouble();
+            segundaNota[i] = scan.nextDouble();
+        }
+        System.out.println("Alunos aprovados");
+
+        double cont = 0;
+        for(int i = 0; i < n;i++){
+            cont = cont + primeiraNota[i] + segundaNota[i];
+        }
+        double media = cont / 2;
+        System.out.println(n);
+    }
 }
--- javacompletonelioalves/src/Vetores/Atividades/mais_velho.java
+++ javacompletonelioalves/src/Vetores/Atividades/mais_velho.java
@@ -1,4 +1,33 @@
 package Vetores.Atividades;
 
+import java.util.Scanner;
+
 public class mais_velho {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Quantas pessoas você vai digitar? ");
+        int n = scan.nextInt();
+
+        String[] nomes = new String[n];
+        int[] idades = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Dados da " + (i + 1) + " pessoa");
+            System.out.print("Nome: ");
+            nomes[i] = scan.next();
+            System.out.print("Idade: ");
+            idades[i] = scan.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (idades[i] > 0) {
+                System.out.println(idades[i]);
+
+            }
+        }
+    }
+
+
 }
+
