849470f7b1a7da4d81aef6af1f351174ef37c033
JP Ferreira <jotepauder01@gmail.com>
2024-01-16 14:47:35 -0300

    Updaet curso Java

---
+++ javacompletonelioalves/src/TopicosEspeciaisDataHora/exemploTeste.java
@@ -0,0 +1,16 @@
+package TopicosEspeciaisDataHora;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class exemploTeste {
+    public static void main(String[] args) {
+
+        LocalDate dataHoje = LocalDate.now(); // Data de hoje
+        LocalDateTime dataHojeHora = LocalDateTime.now(); // Data de hoje e horario
+
+        System.out.println(dataHoje);
+        System.out.println(dataHojeHora);
+    }
+}
