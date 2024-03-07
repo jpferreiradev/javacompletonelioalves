3d2a9fee46ea9441f975fe5240f97d809c776736
JP Ferreira <jotepauder01@gmail.com>
2024-03-07 16:40:28 -0300

    Update curso Java

--- javacompletonelioalves/src/OrientacaoObjetos/SegundaParte/TratamentoExcecoes/AtividadeExemplo/solucaoMuitoRuim/application/Program.java
+++ javacompletonelioalves/src/OrientacaoObjetos/SegundaParte/TratamentoExcecoes/AtividadeExemplo/solucaoMuitoRuim/application/Program.java
@@ -32,17 +32,36 @@ public class Program {
             System.out.print("Check-out data (dd/MM/yyyy: ");
             checkOut = sdf.parse(scan.next());
 
-            Date agora = new Date();
-            if(checkIn.before(agora) || checkOut.before(agora)){
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            } else if(!checkOut.after(checkIn)){
-                System.out.println("Error in reservation: Check-out date must be after check-in date");
-            } else{
-                reserva.updateData(checkIn,checkOut);
-                System.out.println("Reserva:" + reserva);
-            }
-
-
+            reserva.updateData(checkIn, checkOut);
+            System.out.println("Reserva:" + reserva);
         }
     }
+
 }
+/*
+  Scanner scan = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Numero quarto:");
+        int numero = scan.nextInt();
+        System.out.print("Check-in data (dd/MM/yyyy): ");
+        Date checkIn = sdf.parse(scan.next());
+        System.out.print("Check-out data (dd/MM/yyyy: ");
+        Date checkOut = sdf.parse(scan.next());
+
+        if (!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        } else {
+            Reserva reserva = new Reserva(numero, checkIn, checkOut);
+            System.out.println("Reserva: " + reserva);
+
+            System.out.println();
+            System.out.println("Entre com a data da reserva para atualizar:");
+            System.out.print("Check-in data (dd/MM/yyyy): ");
+            checkIn = sdf.parse(scan.next());
+            System.out.print("Check-out data (dd/MM/yyyy: ");
+            checkOut = sdf.parse(scan.next());
+
+            reserva.updateData(checkIn, checkOut);
+            System.out.println("Reserva:" + reserva);
+ */
