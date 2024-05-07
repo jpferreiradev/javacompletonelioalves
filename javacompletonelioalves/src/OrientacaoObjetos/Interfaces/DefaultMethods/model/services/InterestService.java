b2a51ff7c078d21dd7e0dbceaf34cb1d7b031213
JP Ferreira <jotepauder01@gmail.com>
2024-05-07 15:12:44 -0300

    Update curso Java

---
+++ javacompletonelioalves/src/OrientacaoObjetos/Interfaces/DefaultMethods/model/services/InterestService.java
@@ -0,0 +1,13 @@
+package OrientacaoObjetos.Interfaces.DefaultMethods.model.services;
+
+public interface InterestService {
+
+    double getInterestRate();
+
+    default double payment(double amount, int months) {
+        if (months < 1) {
+            throw new IllegalArgumentException("Months must be at least 1");
+        }
+        return amount * Math.pow(1.0 + getInterestRate() / 100.0, months);
+    }
+}
---
+++ javacompletonelioalves/src/OrientacaoObjetos/Interfaces/DefaultMethods/model/services/UsaInterestService.java
@@ -0,0 +1,17 @@
+package OrientacaoObjetos.Interfaces.DefaultMethods.model.services;
+
+public class UsaInterestService implements InterestService {
+
+    private double interestRate;
+
+    public UsaInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+
+}
