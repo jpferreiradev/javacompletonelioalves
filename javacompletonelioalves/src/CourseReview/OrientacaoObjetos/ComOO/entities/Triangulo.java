9a79b3f4cfe88bab333f09e5bef2a6264ab5da9b
JP Ferreira <jotepauder01@gmail.com>
2025-01-20 19:06:03 -0300

    Update comecando OO

--- javacompletonelioalves/src/CourseReview/OrientacaoObjetos/ComOO/entities/Triangulo.java
+++ javacompletonelioalves/src/CourseReview/OrientacaoObjetos/ComOO/entities/Triangulo.java
@@ -1,4 +1,16 @@
 package CourseReview.OrientacaoObjetos.ComOO.entities;
 
 public class Triangulo {
+
+    // Atributos
+    public double a;
+    public double b;
+    public double c;
+
+    public double area() {
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    }
+
 }
--- javacompletonelioalves/src/CourseReview/OrientacaoObjetos/SemOO/application/Program.java
+++ javacompletonelioalves/src/CourseReview/OrientacaoObjetos/SemOO/application/Program.java
@@ -6,33 +6,7 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
-        Locale.setDefault(Locale.US);
-        Scanner scan = new Scanner(System.in);
 
-        double xA,xB,xC,yA,yB,yC;
-        System.out.println("Entre com os valores de X: ");
-        xA = scan.nextDouble();
-        xB = scan.nextDouble();
-        xC = scan.nextDouble();
-        System.out.println("Entre com os valores de Y: ");
-        yA = scan.nextDouble();
-        yB = scan.nextDouble();
-        yC = scan.nextDouble();
-
-        double p = (xA + xB+ xC) / 2.0;
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
-
-        p = (yA + yB+ yC) / 2.0;
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
-
-        System.out.printf("Triângulo área X: %.4f%n", areaX);
-        System.out.printf("Triângulo área Y: %.4f%n", areaY);
-
-        if(areaX > areaY){
-            System.out.println("Area do triângulo X é maior" );
-        } else{
-            System.out.println("Area do triângulo Y é maior");
-        }
 
 
     }
