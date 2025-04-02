648e250f2b4dcaa7d8bcfb092fd6221f6a144d8b
JP Ferreira <jotepauder01@gmail.com>
2025-04-01 22:00:21 -0300

    Update curso revisão vetores

--- javacompletonelioalves/src/CourseReview/JavaIntermediario/Vetores/ExemploAltura.java
+++ javacompletonelioalves/src/CourseReview/JavaIntermediario/Vetores/ExemploAltura.java
@@ -6,12 +6,29 @@ import java.util.Scanner;
 public class ExemploAltura {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US); // O Locale, precisa ficar aqui;
         Scanner scan  = new Scanner(System.in);
-        Locale.setDefault(Locale.US);
 
+
+
+        System.out.print("Informe a quantidade de alturas: ");
         int quantidade = scan.nextInt();
 
+        double[] alturas = new double[quantidade];
 
+        for(int i = 0; i < alturas.length; i++) {
+            System.out.println("Informe a " + (i+1) + "altura");
+            alturas[i] = scan.nextDouble();
+        }
+
+        double soma = 0.0;
+        for(int i =0; i < alturas.length; i++) {
+            soma += alturas[i];
+        }
+
+        double media = soma / quantidade;
+
+        System.out.printf("Altura média: " + String.format("%.2f", media));
 
     }
 }
---
+++ javacompletonelioalves/src/CourseReview/JavaIntermediario/Vetores/ExemploProduto/application/Program.java
@@ -0,0 +1,36 @@
+package CourseReview.JavaIntermediario.Vetores.ExemploProduto.application;
+
+import CourseReview.JavaIntermediario.Vetores.ExemploProduto.entities.Produto;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        Produto[] vetor = new Produto[n];
+
+        for(int i = 0; i < vetor.length;i++){
+            scan.nextLine(); // Aqui foi porque a quebra de linha ficou pendente no nextInt;
+            String nome = scan.next();
+            double preco = scan.nextDouble();
+            vetor[i] = new Produto(nome, preco); // Instanciar um novo produto e o vetor na posição I vai apontar para o objeto;
+        }
+
+        double soma = 0.0;
+
+        // Soma de todos os preços
+        for(int i =0; i < vetor.length; i++) {
+            soma += vetor[i].getPreco(); // Estudar isso aqui
+        }
+
+        double media = soma / vetor.length;
+
+        System.out.printf("Preço médio: " + String.format("%.2f", media));
+    }
+}
