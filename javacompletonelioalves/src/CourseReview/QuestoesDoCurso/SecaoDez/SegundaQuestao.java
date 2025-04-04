b050174a1c726fb3ab33b79af467704deb32c9ac
JP Ferreira <jotepauder01@gmail.com>
2025-04-03 22:07:11 -0300

    Segunda questao pronta

---
+++ javacompletonelioalves/src/CourseReview/QuestoesDoCurso/SecaoDez/SegundaQuestao.java
@@ -0,0 +1,46 @@
+package CourseReview.QuestoesDoCurso.SecaoDez;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SegundaQuestao {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Quantos números você vai digitar? ");
+        int numero = scan.nextInt();
+
+        double[] notas = new double[numero];
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.print("Digite um número: ");
+            notas[i] = scan.nextDouble();
+
+        }
+
+        System.out.print("VALORES =  ");
+        for (int i = 0; i < notas.length; i++) {
+            System.out.printf(" " + notas[i]);
+        }
+
+        double soma = 0.0;
+        for (int i = 0; i < notas.length; i++) {
+            soma += notas[i];
+        }
+        double media = soma / notas.length;
+
+
+        System.out.println();
+        System.out.println("SOMA = " + soma);
+        System.out.println("MEDIA = " + media);
+    }
+}
+
+/*
+/*
+        for (double n : notas) {
+            System.out.printf(" " + n);
+        }
+        */
